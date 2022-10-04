aa7040362616beb56ef5749c3596e349cb4a035c
mungyutae <8jadeshape@gmail.com>
2022-10-04 16:05:45 +0900

    2022/10/04/canvasAddImgAndText

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+const textInput = document.querySelector("#text");
+const fileInput = document.querySelector("#file");
+const eraserBtn = document.querySelector("#eraser-btn");
+const initBtn = document.querySelector("#init-btn");
+const fillBtn = document.querySelector("#fill-btn");
 const colorOption = Array.from(document.querySelectorAll(".color-option"));
-// document.method: HtmlCollection -> Array.from change
+// document.method: HtmlCollection -> Array.from change,
+// a lot of class -> querySelectorAll 
 const color = document.querySelector("#color");
 const canvas = document.querySelector("canvas");
 const lineWidth = document.querySelector("#line-width");
@@ -37,17 +43,21 @@ context.fill();
 
 // Math.random(): 무작위 수
 // Math.floor(): 올림
+context.lineCap = "round";
 context.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
+
+
 // JS always gives event, event takes many info
 function onMouseMove(event) {
-    if(isPainting){
+    if (isPainting) {
         context.lineTo(event.offsetX, event.offsetY);
         context.stroke();
         return;
     }
     context.moveTo(event.offsetX, event.offsetY);
-    
+
 }
 
 function startPaint() {
@@ -62,18 +72,71 @@ function cancelPaint() {
 function lineWidthChange(event) {
     context.lineWidth = event.target.value;
 }
-
+// rangebox color
 function colorChange(event) {
     context.strokeStyle = event.target.value;
     context.fillStyle = event.target.value;
 }
-
+//example painting board color
 function colorClick(event) {
     context.strokeStyle = event.target.dataset.color;
     context.fillStyle = event.target.dataset.color;
     color.value = event.target.dataset.color;
 }
 
+// Default: false = 그리기 모드
+function onFillClick() {
+    if (isFilling) {
+        isFilling = false;
+        fillBtn.innerText = "Fill"; // changing btn use boolean
+    }
+    else {
+        isFilling = true;
+        fillBtn.innerText = "Draw";
+    }
+}
+// isFilling = true : 채우기 모드
+function onCanvasClick() {
+    if (isFilling) {
+        context.fillRect(0, 0, 800, 800);
+    }
+}
+
+function init() {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, 800, 800);
+}
+
+function eraserClick() {
+    context.strokeStyle = "white";
+    isFilling = false;
+    fillBtn.innerText = "Fill";
+}
+
+function fileChange(event) {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image(); // === <img src=""
+    image.src = url;
+    image.onload = function () {
+        context.drawImage(image, 0, 0);
+        fileInput.value = null;
+    }
+}
+
+function onDoubleClick(event) {
+    const text = textInput.value;
+    if (text !== "") {
+        context.save(); // All Select of context saved 
+        context.lineWidth = 1;
+        context.font = "30px ,Georgia"
+        context.strokeText(text, event.offsetX, event.offsetY);
+        context.restore();
+    }// Saved context changed 
+
+}
+
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMouseMove);
 canvas.addEventListener("mousedown", startPaint);
 canvas.addEventListener("mouseup", cancelPaint);
@@ -82,6 +145,15 @@ canvas.addEventListener("mouseleave", cancelPaint);
 // 벗어나서 mouseup()이 실행이 안됨 -> mouseleave()를 사용
 lineWidth.addEventListener("change", lineWidthChange);
 color.addEventListener("change", colorChange);
-
+// Color board
 console.log(colorOption);
 colorOption.forEach(color => color.addEventListener("click", colorClick));
+// Fill
+fillBtn.addEventListener("click", onFillClick);
+canvas.addEventListener("click", onCanvasClick);
+// Eraser
+initBtn.addEventListener("click", init);
+eraserBtn.addEventListener("click", eraserClick);
+// file
+fileInput.addEventListener("change", fileChange);
+
